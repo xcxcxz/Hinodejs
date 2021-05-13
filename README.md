1f82a80c39ec259c8687c4fca8aa91ed57d82e21
zzo <xcxcxz@naver.com>
2021-05-13 13:38:28 +0900

    hello node!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello World for NODE.js
+node.js 를 이용한 서버 만들기
---
+++ index.js
@@ -0,0 +1,13 @@
+//index.js
+
+var express = require('express');
+var app =  express();
+
+app.get('/', function(req,res){
+    res.send('Hello World!');
+});
+
+var port = 3000;
+app.listen(port, function(){
+    console.log('server on! http://localhost:' + port);
+});
